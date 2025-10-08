05e754b30031d88b2eac2006538fe874b4c28fff
calebe22061 <calebe.22061@obhs.school.nz>
2025-10-08 15:00:16 +1300

    day 3

--- sams_sandwhich.py
+++ sams_sandwhich.py
@@ -24,54 +24,55 @@ def force_number(message,lower,upper):
             print('Error - only type in numbers please.')
     return num
 
+def force_list(message,lower,upper):
+    while True:  # Forces the user to enter a valid number
+        try:
+            num=int(input(message))  # Convert input to an integer
+            if num>=lower and num<=upper:  # Check if number is within the valid range
+                return num  # Valid number, exit loop
+            else:
+                print(f"Invalid number, please enter in {lower} - {upper}")
+        except ValueError:  # This will pick up any errors if an integer was not entered
+            print('Error - only type in numbers please.')
+    return num
+
+def print_lists(list,item):
+    count = 0
+    print(f"We have the following {item}")
+    while count < len(list):
+        print(count+1," ", list[count])
+        count+=1
+    return
+
 
 def bread_selection():
     bread_list = ["White", "Brown", "Italian", "Granary"]
-    count=0
-    print("We have the following breads:")
-    while count < len(bread_list):
-        print(count+1," ", bread_list[count])
-        count += 1
-    bread_selected=int(input("which bread did you want? Enter a number: "))
+    print_lists(bread_list,"breads")
+    bread_selected=force_list("Which bread did you want? Enter a number: ",1,len(bread_list))
     return bread_list[bread_selected-1]
 
 def meat_selection():
     meat_list = ["Chicken", "Pork", "Beef", "Meatballs", "Duck"]
-    count=0
-    print("We have the following meats:")
-    while count < len(meat_list):
-        print(count+1," ", meat_list[count])
-        count += 1
-    meat_selected=int(input("which meat did you want? Enter a number: "))
+    print_lists(meat_list,"meats")
+    meat_selected=force_list("which meat did you want? Enter a number: ",1,len(meat_list))
     return meat_list[meat_selected-1]
 
 def cheese_selection():
     cheese_list = ["Cheddar", "Mozzerella", "Swiss", "American", "Blue"]
-    count=0
-    print("We have the following cheeses:")
-    while count < len(cheese_list):
-        print(count+1," ", cheese_list[count])
-        count += 1
-    cheese_selected=int(input("which cheese did you want? Enter a number: "))
+    print_lists(cheese_list,"cheeses")
+    cheese_selected=force_list("which cheese did you want? Enter a number: ",1,len(cheese_list))
     return cheese_list[cheese_selected-1]
 
 def dressing_selection():
     dressing_list = ["Mayonaise", "Ranch", "Mustard", "Butter", "Pesto"]
-    count=0
-    print("We have the following dressings:")
-    while count < len(dressing_list):
-        print(count+1," ", dressing_list[count])
-        count += 1
-    dressing_selected=int(input("which dressing did you want? Enter a number: "))
+    print_lists(dressing_list,"dressings")
+    dressing_selected=force_list("which dressing did you want? Enter a number: ",1,len(dressing_list))
     return dressing_list[dressing_selected-1]
+
 
 def salads_selection():
     salad_list = ["Lettuce", "Tomato", "Carrot", "Cucumber", "Onions"]
-    count=0
-    print("We have the following salds, you can have as many as you want")
-    while count <len(salad_list):
-        print(count," ",salad_list[count])
-        count +=1
+    print_lists(salad_list,"salads")
     print("Press ENTER when you have finished choosing your salads")
     salads_added = "" #will hold a string of more than one item
     selected_salad= " " #prompts the user to enter a number in to select a salad
@@ -102,22 +103,25 @@ def output_textfile(first_name,phone_number,sandwhich_order):
 
 
 #main program
-today=datetime.datetime.now()
-print("Welcome to Sam's Sandwhich Shop")
-first_name=force_name("Please enter in your first name: ",2,15)
-phone_number=force_number("Please enter your phone number: ",0,999999999999999)
-bread_choice=bread_selection() #creating a variable that calls up  the bread function and returns their choice
-meat_choice=meat_selection() #creating a variable that calls up  the meat function and returns their choice
-cheese_choice=cheese_selection() #creating a variable that calls up  the cheese function and returns their choice
-dressing_choice=dressing_selection() #creating a variable that calls up  the cheese function and returns their choice
-salad_choice=salads_selection()
-sandwhich_order=[]
-#adding selected items to a list
-sandwhich_order.append(first_name)
-sandwhich_order.append(phone_number)
-sandwhich_order.append(f'''Your selected bread: {bread_choice}''')
-sandwhich_order.append(f'''Your selected meat: {meat_choice}''')
-sandwhich_order.append(f'''Your selected cheese: {cheese_choice}''')
-sandwhich_order.append(f'''Your selected dressing: {dressing_choice}''')
-sandwhich_order.append(f'''Your selected salads: {salad_choice}''')
-output_textfile(first_name,phone_number,sandwhich_order)
+def main_program():
+    today=datetime.datetime.now()
+    print("Welcome to Sam's Sandwhich Shop")
+    first_name=force_name("Please enter in your first name: ",2,15)
+    phone_number=force_number("Please enter your phone number: ",0,999999999999999)
+    bread_choice=bread_selection() #creating a variable that calls up  the bread function and returns their choice
+    meat_choice=meat_selection() #creating a variable that calls up  the meat function and returns their choice
+    cheese_choice=cheese_selection() #creating a variable that calls up  the cheese function and returns their choice
+    dressing_choice=dressing_selection() #creating a variable that calls up  the cheese function and returns their choice
+    salad_choice=salads_selection()
+    sandwhich_order=[]
+    #adding selected items to a list
+    sandwhich_order.append(first_name)
+    sandwhich_order.append(phone_number)
+    sandwhich_order.append(f'''Your selected bread: {bread_choice}''')
+    sandwhich_order.append(f'''Your selected meat: {meat_choice}''')
+    sandwhich_order.append(f'''Your selected cheese: {cheese_choice}''')
+    sandwhich_order.append(f'''Your selected dressing: {dressing_choice}''')
+    sandwhich_order.append(f'''Your selected salads: {salad_choice}''')
+    output_textfile(first_name,phone_number,sandwhich_order)
+
+main_program()
